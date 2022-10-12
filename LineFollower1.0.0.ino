dd853c15ad2e26e16ce1d3ccabe6e7f86a30c5a9
rbkenmiles <115633180+rbkenmiles@users.noreply.github.com>
2022-10-12 20:11:14 +0530

    Updated the code with forward movement
    Possibel errors not improved with

--- LineFollower1.0.0.ino
+++ LineFollower1.0.0.ino
@@ -1,3 +1,39 @@
 //*************MOTOR PINS*********//
-const int MOTOR_A1 7;
-const int Motor_A1 3;
+//Declare the Pins DONT SIMPLY COPY PASTE 
+const int in1 ;
+const int in2 ;
+const int in3 ;
+const int in4 ;
+const int ENA ;
+const int ENB ;
+//SENSOR PINS
+const int ir1 ;
+const int ir2 ;
+const int ir3 ;
+const int ir4 ;
+const int ir5 ;
+
+void setup()
+{
+pinMode(in1,OUTPUT);
+pinMode(in2,OUTPUT);
+pinMode(in3,OUTPUT);
+pinMode(in4,OUTPUT);
+pinMode(ENA,OUTPUT);
+pinMode(ENB,OUTPUT);
+pinMode(ir1, INPUT);
+pinMode(ir2, INPUT);
+pinMode(ir3, INPUT);
+pinMode(ir4, INPUT);
+pinMode(ir5, INPUT);
+
+
+}
+void main(){
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+
+//Makes the robot moces forward
+}
