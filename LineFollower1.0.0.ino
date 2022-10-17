dfe846e632684f307884b9711c474f383ae0d262
ROBIN BIJO <69898405+RobinBijo@users.noreply.github.com>
2022-10-17 17:24:55 +0530

    Main Motion code
    Added code for moving Forward Backward Left and right(slight and hard)

--- LineFollower1.0.0.ino
+++ LineFollower1.0.0.ino
@@ -1,34 +1,88 @@
-//*************MOTOR PINS*********//
-//Declare the Pins DONT SIMPLY COPY PASTE 
-const int in1 ;
-const int in2 ;
-const int in3 ;
-const int in4 ;
-const int ENA ;
-const int ENB ;
-//SENSOR PINS
-const int ir1 ;
-const int ir2 ;
-const int ir3 ;
-const int ir4 ;
-const int ir5 ;
-
-void setup()
-{
-pinMode(in1,OUTPUT);
-pinMode(in2,OUTPUT);
-pinMode(in3,OUTPUT);
-pinMode(in4,OUTPUT);
-pinMode(ENA,OUTPUT);
-pinMode(ENB,OUTPUT);
-pinMode(ir1, INPUT);
-pinMode(ir2, INPUT);
-pinMode(ir3, INPUT);
-pinMode(ir4, INPUT);
-pinMode(ir5, INPUT);
-
-
+void sensorCheck(){
+s1 = digitalRead(ir1);
+s2 = digitalRead(ir2);
+s3 = digitalRead(ir3);
+s4 = digitalRead(ir4);
+s5 = digitalRead(ir5);
 }
+
+/*
+MAIN MOTION PARTA for v1.0.0
+*/
+void moveForward(){
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+}
+void hardLeft(){
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+}
+void hardRight(){
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+}
+void slightLeft(){
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+}
+void slightRight(){
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,LOW);
+}
+
+void fullStop(){
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,LOW);
+}
+
+
+/*
+FOR MAZE FINISHER
+void uTurn(){
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+}
+
+void move1Inch(){
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+	delay(500);// Check the delay properly
+	
+}
+
+*/
+/*
+---------------------------
+//Only for Debug Purpose
+
+void moveBackward(){
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+}
+-----------------------------
+*/
+
+
+
+
 void main(){
   digitalWrite(in1,HIGH);
   digitalWrite(in2,LOW);
